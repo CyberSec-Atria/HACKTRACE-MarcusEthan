0eee7e0522c34779dadb773a38bd7fb95cae6a63
Shivam Malge <147587521+ShivamMalge@users.noreply.github.com>
2025-03-12 16:13:44 +0530

    Update testimonialsPage.tsx

--- components/testimonialsPage.tsx
+++ components/testimonialsPage.tsx
@@ -25,6 +25,7 @@ export default function Testimonials() {
 
     return (
         <section className="py-16 md:py-24">
+            <div className="hidden">its hidden</div>
         <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold mb-12 text-center">Testimonials</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
